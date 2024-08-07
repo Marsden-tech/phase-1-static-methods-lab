0d7e7c887166d719d7e912cf925a79995213e841
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-08-07 13:14:04 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\s'-]/g, "");
+  }
+
+  static titleize(str) {
+    const wordsToIgnore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return str.split(' ')
+      .map((word, index) => {
+        if (index === 0 || !wordsToIgnore.includes(word.toLowerCase())) {
+          return Formatter.capitalize(word);
+        }
+        return word;
+      })
+      .join(' ');
+  }
 }
